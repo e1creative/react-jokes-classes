8b9d1951c42fb65e9806bc1de6deac4e1daa572b
Jason Takahashi <jasontakahashi@Jasons-MacBook-Pro.local>
2022-11-21 00:36:32 -0800

    Finish Joke.js. . Work on JokeList.js

--- src/Joke.js
+++ src/Joke.js
@@ -1,27 +1,75 @@
 import React from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+// function Joke({ vote, votes, text, id }) {
+//   const upVote = () => vote(id, +1);
+//   const downVote = () => vote(id, -1);
 
-  return (
-    <div className="Joke">
-      <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
+//   return (
+//     <div className="Joke">
+//       <div className="Joke-votearea">
+//         <button onClick={upVote}>
+//           <i className="fas fa-thumbs-up" />
+//         </button>
 
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
+//         <button onClick={downVote}>
+//           <i className="fas fa-thumbs-down" />
+//         </button>
 
-        {votes}
+//         {votes}
+//       </div>
+
+//       <div className="Joke-text">{text}</div>
+//     </div>
+//   );
+// }
+
+
+/**********************************************************************/
+
+
+class Joke extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.vote = this.props.vote
+    this.votes = this.props.votes
+    this.text = this.props.text
+    this.id = this.props.id 
+
+    this.upVote = this.upVote.bind(this);
+    this.downVote = this.downVote.bind(this);
+  }
+
+  upVote() {
+    console.log("UPVOTED - this: ", this)
+    this.vote(this.id, +1);
+  };
+  downVote() {
+    console.log("DOWNVOTED - this: ", this)
+    this.vote(this.id, -1);
+  };
+
+  render() {
+
+    return (
+      <div className="Joke">
+        <div className="Joke-votearea">
+          <button onClick={this.upVote}>
+            <i className="fas fa-thumbs-up" />
+          </button>
+  
+          <button onClick={this.downVote}>
+            <i className="fas fa-thumbs-down" />
+          </button>
+  
+          {this.votes}
+        </div>
+  
+        <div className="Joke-text">{this.text}</div>
       </div>
-
-      <div className="Joke-text">{text}</div>
-    </div>
-  );
+    );  
+  }
 }
 
 export default Joke;
